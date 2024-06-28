0afca556f21a3e258174bf27bac18ca4e37a6ce4
Xiao-Ling-Script <2806224430@qq.com>
2024-06-28 21:16:27 +0800

    Add files via upload

---
+++ 制作.lua
@@ -0,0 +1,175 @@
+local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")() 
+
+local OpenUI = Instance.new("ScreenGui") 
+local ImageButton = Instance.new("ImageButton") 
+local UICorner = Instance.new("UICorner") 
+OpenUI.Name = "OpenUI" 
+OpenUI.Parent = game.CoreGui 
+OpenUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling 
+ImageButton.Parent = OpenUI 
+ImageButton.BackgroundColor3 = Color3.fromRGB(5, 6, 7) 
+ImageButton.BackgroundTransparency = 0.500 
+ImageButton.Position = UDim2.new(0.0235790554, 0, 0.466334164, 0) 
+ImageButton.Size = UDim2.new(0, 50, 0, 50) 
+ImageButton.Image = "rbxassetid://16060333448" 
+ImageButton.Draggable = true 
+UICorner.CornerRadius = UDim.new(0, 200) 
+UICorner.Parent = ImageButton 
+ImageButton.MouseButton1Click:Connect(function() 
+  if uihide == false then
+	uihide = true
+	game.CoreGui.ui.Main:TweenSize(UDim2.new(0, 0, 0, 0),"In","Quad",0.4,true)
+else
+	uihide = false
+	game.CoreGui.ui.Main:TweenSize(UDim2.new(0, 560, 0, 319),"Out","Quad",0.4,true)
+		end 
+		
+end)
+
+uihide = false
+
+local win = lib:Window("冬凌中心[主脚本]",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl) 
+  
+ local setup = win:Tab(" ")
+
+ setup:Label("不要卡密(免费)")
+ setup:Label("By：小凌.AEX、囚")
+ 
+setup:Button("点我打开冬凌中心!",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/flyspeed7/XiaoLing-UwU/main/%E5%86%AC%E5%87%8C%E4%B8%AD%E5%BF%83(%E5%B7%B2%E6%81%A2%E5%A4%8D).XiaoLing.Lua"))()
+end)
+
+ local win = lib:Window("内设脚本",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl) 
+  
+ local setup = win:Tab(" ")
+
+ setup:Label("脚本为启动器功能不是太多")
+ setup:Label("By：小凌.AEX")
+ 
+setup:Button("打死声音",function()
+for i, v in pairs(game.Workspace["_WorldOrigin"]:GetChildren()) do
+if v.Name == "CurvedRing" or v.Name == "SlashHit" or v.Name == "SwordSlash" or v.Name == "Sounds" then
+v:Destroy()
+end
+end
+end)
+setup:Colorpicker("更改用户界面颜色",Color3.fromRGB(44, 120, 224), function(t) 
+ lib:ChangePresetColor(Color3.fromRGB(t.R * 255, t.G * 255, t.B * 255)) 
+ end)
+ 
+ 
+tab:Toggle("去除雾气", false, function(state)
+    if state then
+        game.Lighting.FogStart = 3276634343
+        game.Lighting.FogEnd = 3276734343
+    else
+        game.Lighting.FogStart = 0
+    end
+end)
+ 
+tab2:Button("死亡",function() 
+     game.Players.LocalPlayer.Character.Humanoid.Health = 0 
+ end) 
+
+local server = win:Tab("加入服务器/游戏")
+
+server:Button("加入极速传奇",function()
+local game_id = 3101667897
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
+
+server:Button("加入鲨口生求2",function()
+local game_id = 8908228901
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
+
+server:Button("加入监狱人生",function()
+local game_id = 155615604
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
+
+server:Button("加入忍者传奇",function()
+local game_id = 3956818381
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
+
+server:Button("加入Break in (故事)",function()
+local game_id = 1318971886
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
+
+server:Button("加入自然灾害生存游戏",function()
+local game_id = 189707
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
+
+server:Button("加入力量传奇",function()
+local game_id = 3623096087
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
+
+server:Button("加入餐厅大亨2",function()
+local game_id = 3398014311
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
+
+
+--[[
+server:Button("", function() 
+
+end
+
+server:Button("", function() 
+
+end
+
+server:Button("", function() 
+
+end
+
+server:Button("", function() 
+
+end
+]]
+ -- Gui to Lua 
+ -- Version: 3.2 
+  
+ -- Instances:
+ local ScreenGui = Instance.new("ScreenGui") 
+ local FpsLabel = Instance.new("TextLabel")
+ 
+ --Properties:
+ 
+ ScreenGui.Name = "FPSGui" 
+ ScreenGui.ResetOnSpawn = false 
+ ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling 
+ 
+ FpsLabel.Name = "FPSLabel" 
+ FpsLabel.Size = UDim2.new(0, 100, 0, 50) 
+ FpsLabel.Position = UDim2.new(0, 10, 0, 10) 
+ FpsLabel.BackgroundTransparency = 1 
+ FpsLabel.Font = Enum.Font.SourceSansBold 
+ FpsLabel.Text = "帧率: 0" 
+ FpsLabel.TextSize = 20 
+ FpsLabel.TextColor3 = Color3.new(1, 1, 1) 
+ FpsLabel.Parent = ScreenGui 
+  
+ function updateFpsLabel() 
+     local fps = math.floor(1 / game:GetService("RunService").RenderStepped:Wait()) 
+     FpsLabel.Text = "帧率: " .. fps 
+ end 
+  
+  game:GetService("RunService").RenderStepped:Connect(updateFpsLabel) 
+  
+ ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+
+ animateCredits() 
